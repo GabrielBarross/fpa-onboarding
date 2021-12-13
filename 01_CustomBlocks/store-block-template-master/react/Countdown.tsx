5f0853730c8e43d70a1f745284e477d1d2fb5639
Gabriel Barros <gabrielbarros@MacBook-Pro-de-Gabriel.local>
2021-12-13 12:03:21 -0300

    Countdown component created based on the Learning Path Custom Block course

--- 01_CustomBlocks/store-block-template-master/react/Countdown.tsx
+++ 01_CustomBlocks/store-block-template-master/react/Countdown.tsx
@@ -1,16 +1,55 @@
-import React from 'react'
+import React , { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
+import { FormattedMessage } from 'react-intl'
 
-interface CountdownProps {}
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div />
+interface CountdownProps {
+  title: string
+  targetDate: string
 }
+
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+const CSS_HANDLES = ['countdownBox', 'countdown', 'title']
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({title , targetDate = DEFAULT_TARGET_DATE}) => {
+
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  tick(targetDate, setTime)
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const titleText = title || <FormattedMessage id="countdown.title" />
+
+  return ( 
+    <div className={`${handles.countdownBox} c-muted-1 db tc`}>
+      <h1 className={`${handles.title}`}>{titleText}</h1>
+      <p className={`${handles.countdown}`}>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</p>
+    </div>
+  )}
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    title: {
+      title: "Título",
+      type: "string",
+      default: null
+    },
+    targetDate: {
+      title: "Data Final",
+      description: "Data Final do Countdown de teste",
+      type: "string", 
+      default: null
+    }
+  },
 }
 
 export default Countdown
